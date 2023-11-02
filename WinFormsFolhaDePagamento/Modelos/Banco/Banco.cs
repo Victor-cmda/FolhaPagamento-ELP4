9fae62ab4c8ab1a8933302653927156ca31c915c
victo <victor21420@gmail.com>
2023-11-01 23:23:33 -0300

    Inserindo banco de dados

---
+++ WinFormsFolhaDePagamento/Modelos/Banco/Banco.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace WinFormsFolhaDePagamento.Modelos.Banco
+{
+    public static class Banco
+    {
+        public static SqlConnection Open()
+        {
+            string connectionString = @"Data Source=DESKTOP-5Q9Q0NQ\SQLEXPRESS;Initial Catalog=folha_de_pagamento;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
